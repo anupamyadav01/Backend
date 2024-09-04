0653ca81ca160fdd384c3f3b468c1e29e7eb25ca
Anupam Yadav <anupamy571@gmail.com>
2024-09-04 09:43:54 +0530

    url shortner created in backend

--- Day-7( URL Shortner Application )/backend/index.js
+++ Day-7( URL Shortner Application )/backend/index.js
@@ -5,21 +5,60 @@ const app = express();
 const portNo = 8080;
 app.use(express.json());
 
+const isURLValid = (url) => {
+  try {
+    new URL(url);
+    return true;
+  } catch (error) {
+    return false;
+  }
+};
+
 app.post("/shorten", (req, res) => {
-  const urlsDataFromFile = fs.readFileSync("data.json", "utf8");
-  const parsedURLSData = JSON.parse(urlsDataFromFile);
-  console.log(parsedURLSData);
+  const urlFromUser = req.body.url;
+  if (!isURLValid(urlFromUser)) {
+    return res.status(400).json({
+      status: false,
+      message: "Please provide a valid URL",
+    });
+  }
+  const shortURL = nanoid(6);
 
-  const shortURl = nanoid(5);
+  const urlsDataFromFile = JSON.parse(fs.readFileSync("URLs.json", "utf8"));
+  console.log(urlsDataFromFile);
 
-  parsedURLSData[shortURl] = req.body.longURL;
+  // if data is in big amount then it is not good way to do
+  // const urls = { ...urlsDataFromFile, [shortURL]: urlFromUser };
 
-  fs.writeFileSync("data.json", JSON.stringify(parsedURLSData));
-  res.json({
-    status: true,
-    data: `https://${portNo}/shorten/${nanoid(5)}`,
+  // alternate way to do this
+
+  // adding new url in existing urls object
+  urlsDataFromFile[shortURL] = urlFromUser;
+
+  fs.writeFileSync("URLs.json", JSON.stringify(urlsDataFromFile));
+
+  res.send({
+    sucess: true,
+    data: `https://localhost:${portNo}/${shortURL}`,
   });
 });
+
+app.get("/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL;
+  // console.log(shortURL);
+  const urlsDataFromFile = JSON.parse(fs.readFileSync("URLs.json", "utf8"));
+
+  if (urlsDataFromFile[shortURL] === undefined) {
+    return res.status(404).json({
+      sucess: false,
+      message: "Not found",
+    });
+  }
+  res.redirect(urlsDataFromFile[shortURL]);
+
+  res.send("success");
+});
+
 app.listen(portNo, () => {
   console.log(`Server is running at http://localhost:${portNo}`);
 });
