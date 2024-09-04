0beede89c6e73e0fe81c1e9a2ded314f776e6934
Anupam Yadav <anupamy571@gmail.com>
2024-09-04 11:43:42 +0530

    Connection established with MongoDB atlas

--- Day-7( URL Shortner Application )/backend/model/urlModel.js
+++ Day-7( URL Shortner Application )/backend/model/urlModel.js
@@ -1,1 +1,10 @@
+import mongoose from "mongoose";
 
+const urlSchema = new mongoose.Schema({
+  originalURL: {
+    type: String,
+    required: true,
+  },
+});
+
+export const urlModel = mongoose.model("urls", urlSchema);
