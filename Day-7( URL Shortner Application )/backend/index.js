451369bcbded938dd910b27dc5c5248595bf5741
Anupam Yadav <anupamy571@gmail.com>
2024-09-04 11:13:02 +0530

    MVC architecture applied

--- Day-7( URL Shortner Application )/backend/index.js
+++ Day-7( URL Shortner Application )/backend/index.js
@@ -1,67 +1,16 @@
+import dotenv from "dotenv";
 import express from "express";
-import { nanoid } from "nanoid";
-import fs from "fs";
 import cors from "cors";
+import router from "./router/urlRoutes.js";
 const app = express();
-const portNo = 8080;
+
+const portNo = process.env.PORT_NO || 8080;
+dotenv.config();
 
 app.use(express.json());
 app.use(cors({}));
 
-const isURLValid = (url) => {
-  try {
-    new URL(url);
-    return true;
-  } catch (error) {
-    return false;
-  }
-};
-
-app.post("/shorten", (req, res) => {
-  const urlFromUser = req.body.url;
-  if (!isURLValid(urlFromUser)) {
-    return res.status(400).json({
-      status: false,
-      message: "Please provide a valid URL",
-    });
-  }
-  const shortURL = nanoid(6);
-
-  const urlsDataFromFile = JSON.parse(fs.readFileSync("URLs.json", "utf8"));
-  console.log(urlsDataFromFile);
-
-  // if data is in big amount then it is not good way to do
-  // const urls = { ...urlsDataFromFile, [shortURL]: urlFromUser };
-
-  // alternate way to do this
-
-  // adding new url in existing urls object
-  urlsDataFromFile[shortURL] = urlFromUser;
-
-  fs.writeFileSync("URLs.json", JSON.stringify(urlsDataFromFile));
-
-  res.send({
-    sucess: true,
-    shortURL: `https://localhost:${portNo}/${shortURL}`,
-  });
-});
-
-app.get("/:shortURL", (req, res) => {
-  const shortURL = req.params.shortURL;
-  // console.log(shortURL);
-  const urlsDataFromFile = JSON.parse(fs.readFileSync("URLs.json", "utf8"));
-
-  if (urlsDataFromFile[shortURL] === undefined) {
-    return res.status(404).json({
-      sucess: false,
-      message: "Not found",
-    });
-  }
-  res.redirect(urlsDataFromFile[shortURL]);
-
-  res.send("success");
-});
-
+app.use("", router);
 app.listen(portNo, () => {
   console.log(`Server is running at http://localhost:${portNo}`);
 });
